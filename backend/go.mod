cb7a8d258f503befe1065e1b2d3dcc7db1ab8bab
Walter Alvarado <walvdev@gmail.com>
2022-12-01 05:41:55 -0500

    Create basic directory structure

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/walteralv/enronMailApp
+
+go 1.19
---
+++ scripts/get_enron_data.sh
@@ -0,0 +1,15 @@
+# script to get the enron mails data
+
+if [[ ! -d "backend/enron_mail_20110402" ]]; then
+    
+    if [[ ! -f "enron_mail_20110402.tgz" ]]; then
+        echo "Downloading the data from http://download.srv.cs.cmu.edu/\~enron/enron_mail_20110402.tgz ..."
+        curl -O http://download.srv.cs.cmu.edu/\~enron/enron_mail_20110402.tgz 2>&1    
+    fi
+
+    echo "Extracting the data..."
+    tar -C backend/ -xzf enron_mail_20110402.tgz
+    echo "Extracted successfully!"
+else
+    echo "Directory 'enron_mail_20110402' already exists"
+fi
